2c632ecf5740b1150620449f9ac36cb4acadd2b9
Hemanta Pokharel <hemantapkh@gmail.com>
2020-11-06 19:45:54 +0545

    Add config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Specify which file (without .py extension) in the arts folder should be used
+artFile = "art"
+# Speed of art
+speed = 0.005
+# Speed of code printing
+codePrint = True
+codingSpeed = 0.01
+# speed of faceart
+faceArt = False
+faceArtSpeed = 0.1
+# Random color is choosen from the list
+color = ['grey','red','green','yellow','blue','magenta','cyan','white']
+# Change the keys of the dict to change the color codes
+colorCodes = {'①':'grey','②':'red','③':'green','④':'yellow','⑤':'blue','⑥':'magenta','⑦':'cyan','⑧':'white','⑨':'blink','⑩':'none'}
