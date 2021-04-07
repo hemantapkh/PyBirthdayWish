712d1090ef1d0a0d83e0b4bcd34e7af13d06527e
pankajmeena04 <82085637+pankajmeena04@users.noreply.github.com>
2021-04-07 20:49:43 +0530

    Update art.py

--- arts/art.py
+++ arts/art.py
@@ -19,7 +19,7 @@ Copy these codes and paste it before the text for the color your wish
 # Happy Birthday Love
 '''
 https://patorjk.com/software/taag/#p=display&f=Crawford2&t=Happy%20Birthday%0A%20%20%20%20%20%20Name
-Go to the above link and generate art to replace 'Happy Birthday Love' with other text you wish to display.
+Go to the above link and generate art to replace 'Happy Birthday Archi' with other text you wish to display.
 '''
 
 mainArt = \
