c9cc0e5c7a4f2a389d8ccc3f4923ebf62436db9b
pankajmeena07 <81606980+pankajmeena07@users.noreply.github.com>
2021-04-27 13:24:19 +0530

    Update art.py

--- arts/art.py
+++ arts/art.py
@@ -16,7 +16,7 @@ Copy these codes and paste it before the text for the color your wish
 ® = random
 '''
 
-# Happy Birthday Love
+# Happy Birthday archai
 '''
 https://patorjk.com/software/taag/#p=display&f=Crawford2&t=Happy%20Birthday%0A%20%20%20%20%20%20Name
 Go to the above link and generate art to replace 'Happy Birthday Love' with other text you wish to display.
